08046221f316636084f451f3568fa8783627ed92
Beknur Raissov <brown.robert@bk.ru>
2020-12-16 23:55:35 +0600

    user model

--- models/user.go
+++ models/user.go
@@ -1,1 +1,11 @@
 package model
+
+type User struct {
+	Name string `json:"name"`
+	Surname string `json:"surname"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Telephone string `json:"telephone"`
+	Email string `json:"email"`
+	IsValidated bool `json:"is_validated"`
+}
