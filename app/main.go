232c2dc44a002da7fd835be2f5e1a02046850ec7
DYernar <eduisebai.1@gmail.com>
2020-12-16 23:25:28 +0600

    basic server

--- app/main.go
+++ app/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	mainControllers "shanyraq/controllers/main"
 
+	"github.com/rs/cors"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", mainControllers.MainPage)
+
+	handler := cors.Default().Handler(mux)
+
+	err := http.ListenAndServe(":"+port, handler)
+
+	if err != nil {
+		log.Fatal("Listen and serve err: ", err)
+	}
+	fmt.Println("Running on port: " + port)
 }
