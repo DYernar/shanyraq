14cc55bc36e3187e296912d6734e9c2687ad82d0
DYernar <eduisebai.1@gmail.com>
2020-12-20 21:51:43 +0600

    requests handlers

---
+++ controllers/bad_req_handlers.go
@@ -0,0 +1,42 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func NotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(404)
+
+	err := struct {
+		Error string `json:"error"`
+	}{
+		"page not found",
+	}
+	vals, _ := json.Marshal(err)
+	w.Write(vals)
+}
+
+func BadRequest(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(500)
+
+	err := struct {
+		Error string `json:"error"`
+	}{
+		"bad request!",
+	}
+	vals, _ := json.Marshal(err)
+	w.Write(vals)
+}
+
+func NotAuthorized(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusUnauthorized)
+
+	err := struct {
+		Error string `json:"error"`
+	}{
+		"you are not permitted to see this page",
+	}
+	vals, _ := json.Marshal(err)
+	w.Write(vals)
+}
--- controllers/login.go
+++ controllers/login.go
@@ -1,8 +1,32 @@
 package controllers
 
 import (
+	"encoding/json"
 	"net/http"
+	"shanyraq/db"
+	model "shanyraq/models"
 )
 
+var jwtKey = []byte("some secret key")
+
 func Login(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		var user model.User
+		err := json.NewDecoder(r.Body).Decode(&user)
+
+		if err != nil {
+			BadRequest(w, r)
+			return
+		}
+
+		if db.IsValidCredentials(user) {
+			//save session return token
+
+		} else {
+			BadRequest(w, r)
+		}
+
+	} else {
+		BadRequest(w, r)
+	}
 }
